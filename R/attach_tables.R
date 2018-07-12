e6946a4d6577b305216024790376cf0a6d4bcd91
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-07-12 14:02:44 -0700

    list names of tables on attach

--- R/attach_tables.R
+++ R/attach_tables.R
@@ -2,15 +2,23 @@
 #'
 #' @param db A [DBI::DBIConnection-class] object
 #' @param dbname Character. Name of the environment to create
+#' @param quiet Message the names of tables attached? Defaults to `TRUE`
 #'
 #' @return Invisibly returns character vector of table names.
 #'
 #' @export
-attach_database <- function(db, dbname) {
+attach_database <- function(db, dbname, quiet = FALSE) {
   db_tables <- DBI::dbListTables(db)
   e <- new.env(parent = emptyenv())
   lapply(db_tables, attach_single_table, db = db, env = e)
   attach(e, name = dbname)
+
+  if (!quiet) {
+    msg_head <- paste("Attached", length(db_tables), "tables:\n")
+    tbl_strings <- paste("-", db_tables, "\n")
+    message(msg_head, tbl_strings)
+  }
+
   invisible(db_tables)
 }
 
